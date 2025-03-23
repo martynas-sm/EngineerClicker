579601ef7c4e9bcede79903467c52775088ea171
ignas24 <ignaspyrantas@gmail.com>
2025-03-24 01:22:33 +0200

    Added data saving using LocalStorage to save euros

--- src/game/ResourceManager.js
+++ src/game/ResourceManager.js
@@ -4,10 +4,27 @@ export class ResourceManager {
 
   constructor(game) {
     this.game = game;
+    this.loadData(); // Load saved data when the game starts
   }
 
   addEuros(delta) {
     this.euro += delta;
+    this.saveData(); // Save data whenever euros are updated
     this.game.notifyUpdate();
+  }
+
+  saveData() {
+    const data = {
+      euro: this.euro,
+    };
+    localStorage.setItem('gameData', JSON.stringify(data)); // Save data to localStorage
+  }
+
+  loadData() {
+    const savedData = localStorage.getItem('gameData');
+    if (savedData) {
+      const data = JSON.parse(savedData);
+      this.euro = data.euro || 0; // Load euro value or default to 0
+    }
   }
 }
